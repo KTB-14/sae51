870d518aa5579b60b80519c7de200cc196612a45
Brook <brook-tamrat.kifle@univ-rouen.fr>
2025-09-16 16:27:32 +0200

    modif test

--- genMV.sh
+++ genMV.sh
@@ -130,8 +130,8 @@
 # FIN SCRIPT ETAPE 1,2 ---------------------------------------------------------------
 
 
-#======================================================================================================================================
 
+#======================================================================================================================================
 # 3. Etapes proposées:
 
 # Vérifier via la GUI de VB que la machine démarre bien sur le boot PXE. 
@@ -140,6 +140,139 @@
 
 
 # DEBUT SCRIPT ETAPE 1,2,3 ---------------------------------------------------------------
+
+# echo "[1] Vérification si la VM Debian1 existe déjà"
+# if ! vboxmanage createvm --name Debian1 --ostype Debian13 --register &> /dev/null; then
+#     echo "La VM Debian1 existe déjà, suppression en cours..."
+#     vboxmanage unregistervm Debian1 --delete
+    
+#     echo "Suppression terminée. Recréation de la VM Debian1..."
+#     vboxmanage createvm --name Debian1 --ostype Debian13 --register &> /dev/null
+# else
+#     echo "La VM Debian1 n'existe pas, création en cours..."
+# fi
+
+# echo "[2] Création du disque virtuel de 64 Go"
+# vboxmanage createhd --filename "$HOME/VirtualBox VMs/Debian1/Debian1.vdi" --size 65536 --format VDI  &> /dev/null
+
+# echo "[3] Ajout du controleur de stockage"
+# vboxmanage storagectl Debian1 --name Controller_Sata --add sata --controller IntelAhci 
+
+# echo "[4] Attachement du disque virtuel au controleur"
+# vboxmanage storageattach Debian1 --storagectl Controller_Sata --port 0 --device 0 --type hdd --medium "$HOME/VirtualBox VMs/Debian1/Debian1.vdi" 
+
+# echo "[5] Configuration de la VM (RAM, carte reseau, ordre de boot)"
+# vboxmanage modifyvm Debian1 --memory 4096 --nic1 nat --boot1 net --boot2 disk --boot3 none --boot4 none
+
+
+# #-- Suppression des commandes 6 et 7 (temps d'attente et destruction de la VM)
+# #   car l'objectif est de mettre en place le boot PXE et de demarrer la VM ensuite de verifier qu'elle boot bien en PXE
+
+
+# #== DEBUT PXE =======================================
+
+# #-- Liste des variables utilisées pour la configuration du serveur TFTP interne à VirtualBox
+# #   et le boot PXE de l'installateur Debian
+
+# #-- Lien de téléchargement de l'archive netboot.tar.gz
+# NETBOOT_URL="http://http.us.debian.org/debian/dists/trixie/main/installer-amd64/current/images/netboot/netboot.tar.gz"
+# #   Cette archive contient les fichiers nécessaires pour le boot PXE de l'installateur Debian
+
+# #-- Répertoire TFTP utilisé par VirtualBox
+# TFTP_DIR="$HOME/.config/VirtualBox/TFTP"
+
+# #-- Chemin absolu du répertoire où les fichiers de netboot seront extraits
+# NETBOOT_TAR="$TFTP_DIR/netboot.tar.gz"
+
+
+# #-- Création du répertoire TFTP s'il n'existe pas
+# mkdir -p "$TFTP_DIR"
+
+
+# #-- Téléchargement de l'archive netboot.tar.gz si elle n'existe pas déjà
+# echo "[6] Verification si besoin de téléchargement de l'archive netboot.tar.gz " 
+
+# if test ! -f "$TFTP_DIR/pxelinux.0"; then
+
+#     # test [ condition ] : est une commande qui permet de vérifier une condition
+#     # ! : négation (si la condition est fausse)
+#     # -f : option qui vérifie si le fichier existe et est un fichier régulier (que veut dire fichier regulier ? 
+#     #      un fichier regulier est un fichier qui n'est pas un répertoire, un lien symbolique, ou un autre type spécial de fichier)
+
+#     echo "Téléchargement des fichiers netboot Debian..."  
+
+#     if which curl 2>&1 > /dev/null ; then
+#         curl -L -o "$NETBOOT_TAR" "$NETBOOT_URL"
+#         # -L : permet de suivre les redirections (si l'URL redirige vers une autre URL)
+#         # -o : permet de choisir le nom du fichier de sortie
+#     elif which wget 2>&1 > /dev/null ; then
+#         wget -O "$NETBOOT_TAR" "$NETBOOT_URL"
+#         # -O : permet de choisir le nom du fichier de sortie
+#     else
+#         echo "!!! Les commandes 'curl' ou 'wget' ne sont pas installées."
+#         echo " Veuillez les installer et réessayer"
+#         exit 1
+#     fi    
+
+#     echo "Extraction de netboot.tar.gz..."
+#     tar -xzf "$NETBOOT_TAR" -C "$TFTP_DIR"
+#     # -x : extraire
+#     # -z : passer par gzip (parce que c’est .tar.gz)
+#     # -f = file indique le nom du fichier à extraire (ici "$NETBOOT_TAR")
+#     # -C "$TFTP_DIR" = changer de repertoire et extraire directement dans ce dossier (ici "$TFTP_DIR")
+    
+#     rm -f "$NETBOOT_TAR"
+#     # -f : forcer la supression
+# fi
+
+# #-- Vérification finale
+# if test ! -f "$TFTP_DIR/pxelinux.0" ; then
+#     echo "!!! pxelinux.0 introuvable après extraction."
+#     exit 1
+# fi
+
+# #-- Création du lien <VM>.pxe avec pxelinux.0
+# ln -sf "pxelinux.0" "$TFTP_DIR/$vm_name.pxe"
+
+#     # ln : Création d’un lien symbolique vers pxelinux.0
+#     # -s : lien symbolique (comme un raccourci)
+#     # -f : force la création (écrase l’ancien lien si présent)
+#     # Donc chaque VM aura son propre fichier de boot ($vm_name.pxe)
+#     # "pxelinux.0" est la cible réelle (le fichier du boot PXE).
+#     # "$TFTP_DIR/$vm_name.pxe" est le nom du lien symbolique qu’on crée.
+
+# # FIN PXE =======================================
+
+# echo "[7] Démarrage de la VM Debian1 en mode GUI"
+# vboxmanage startvm Debian1 --type gui
+
+# FIN SCRIPT ETAPE 1,2,3 ---------------------------------------------------------------
+
+
+
+#======================================================================================================================================
+# 4. Etapes proposées:
+
+# Ajouter dans le script la gestion d'arguments. Le script devra prendre des arguments (1 ou 2, selon le cas). 
+
+# Le premier argument a donner sera une lettre, décrivant l'action souhaitée :
+#   — L pour lister l'ensemble des machines enregistrées dans VB
+#   — N pour ajouter une nouvelle machine.
+#   — S pour supprimer une machine.
+#   — D pour démarrer une machine.
+#   — A pour arreter une machine.
+
+# Le 2e argument sera le nom de la machine concernée, pour les commandes N, S, D, A.
+# Les machines créées auront les mêmes caractéristiques que précédemment. 
+# La taille de la RAM et du DD seront données en tête du script via des variables, de façon à être
+# facilement modifiable.
+# Par exemple :
+#   RAM=4096 
+
+# DEBUT SCRIPT ETAPE 1,2,3,4 ---------------------------------------------------------------
+
+action="$1"
+vm_name="$2"
 
 echo "[1] Vérification si la VM Debian1 existe déjà"
 if ! vboxmanage createvm --name Debian1 --ostype Debian13 --register &> /dev/null; then
@@ -164,86 +297,36 @@ vboxmanage storageattach Debian1 --storagectl Controller_Sata --port 0 --device 
 echo "[5] Configuration de la VM (RAM, carte reseau, ordre de boot)"
 vboxmanage modifyvm Debian1 --memory 4096 --nic1 nat --boot1 net --boot2 disk --boot3 none --boot4 none
 
-
-#-- Suppression des commandes 6 et 7 (temps d'attente et destruction de la VM)
-#   car l'objectif est de mettre en place le boot PXE et de demarrer la VM ensuite de verifier qu'elle boot bien en PXE
-
-
-#== DEBUT PXE =======================================
-
-#-- Liste des variables utilisées pour la configuration du serveur TFTP interne à VirtualBox
-#   et le boot PXE de l'installateur Debian
-
-#-- Lien de téléchargement de l'archive netboot.tar.gz
 NETBOOT_URL="http://http.us.debian.org/debian/dists/trixie/main/installer-amd64/current/images/netboot/netboot.tar.gz"
-#   Cette archive contient les fichiers nécessaires pour le boot PXE de l'installateur Debian
-
-#-- Répertoire TFTP utilisé par VirtualBox
 TFTP_DIR="$HOME/.config/VirtualBox/TFTP"
-
-#-- Chemin absolu du répertoire où les fichiers de netboot seront extraits
 NETBOOT_TAR="$TFTP_DIR/netboot.tar.gz"
-
-
-#-- Création du répertoire TFTP s'il n'existe pas
 mkdir -p "$TFTP_DIR"
-
-
-#-- Téléchargement de l'archive netboot.tar.gz si elle n'existe pas déjà
 echo "[6] Verification si besoin de téléchargement de l'archive netboot.tar.gz " 
-
-if test ! -f "$NETBOOT_TAR"; then
-    # test [ condition ] : est une commande qui permet de vérifier une condition
-    # ! : négation (si la condition est fausse)
-    # -f : option qui vérifie si le fichier existe et est un fichier régulier (que veut dire fichier regulier ? 
-    #      un fichier regulier est un fichier qui n'est pas un répertoire, un lien symbolique, ou un autre type spécial de fichier)
-
-    echo "Téléchargement de l'archive netboot.tar.gz..."  
-
+if test ! -f "$TFTP_DIR/pxelinux.0"; then
+    echo "Téléchargement des fichiers netboot Debian..."  
     if which curl 2>&1 > /dev/null ; then
         curl -L -o "$NETBOOT_TAR" "$NETBOOT_URL"
-        # -L : permet de suivre les redirections (si l'URL redirige vers une autre URL)
-        # -o : permet de choisir le nom du fichier de sortie
     elif which wget 2>&1 > /dev/null ; then
         wget -O "$NETBOOT_TAR" "$NETBOOT_URL"
-        # -O : permet de choisir le nom du fichier de sortie
     else
         echo "!!! Les commandes 'curl' ou 'wget' ne sont pas installées."
         echo " Veuillez les installer et réessayer"
         exit 1
     fi    
-
     echo "Extraction de netboot.tar.gz..."
     tar -xzf "$NETBOOT_TAR" -C "$TFTP_DIR"
-    # -x : extraire
-    # -z : passer par gzip (parce que c’est .tar.gz)
-    # -f = file indique le nom du fichier à extraire (ici "$NETBOOT_TAR")
-    # -C "$TFTP_DIR" = changer de repertoire et extraire directement dans ce dossier (ici "$TFTP_DIR")
-    
     rm -f "$NETBOOT_TAR"
-    # -f : forcer la supression
 fi
 
-#-- Vérification finale
 if test ! -f "$TFTP_DIR/pxelinux.0" ; then
     echo "!!! pxelinux.0 introuvable après extraction."
     exit 1
 fi
 
-#-- Création du lien <VM>.pxe avec pxelinux.0
 ln -sf "pxelinux.0" "$TFTP_DIR/$vm_name.pxe"
-
-    # ln : Création d’un lien symbolique vers pxelinux.0
-    # -s : lien symbolique (comme un raccourci)
-    # -f : force la création (écrase l’ancien lien si présent)
-    # Donc chaque VM aura son propre fichier de boot ($vm_name.pxe)
-    # "pxelinux.0" est la cible réelle (le fichier du boot PXE).
-    # "$TFTP_DIR/$vm_name.pxe" est le nom du lien symbolique qu’on crée.
-
-# FIN PXE =======================================
 
 echo "[7] Démarrage de la VM Debian1 en mode GUI"
 vboxmanage startvm Debian1 --type gui
 
-# FIN SCRIPT ETAPE 1,2,3 ---------------------------------------------------------------
+# FIN SCRIPT ETAPE 1,2,3,4 ---------------------------------------------------------------
 
